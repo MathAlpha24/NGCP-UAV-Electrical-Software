2a9ba27747022ff8a46337b9e689f65c8e6810c0
MathAlpha24 <80738694+MathAlpha24@users.noreply.github.com>
2025-09-26 11:38:29 -0700

    Adding additional comments and fixes

---
+++ boot_scripts/remove_password.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Goal: Remove password for general usage to ensure easier access to raspberry pi.
+#MAJOR NOTE: Security Risk!!!! Be careful of placing information onto any device that has their password removed. 
+#THIS IS JUST FOR TESTING CASES.
+#The end product will have a password, but everything SHOULD run from boot.
