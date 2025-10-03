51c39ce12c31a6efb584b092074c6b91a20664f3
MathAlpha24 <80738694+MathAlpha24@users.noreply.github.com>
2025-10-03 13:19:41 -0700

    bug fixing installation script

--- install_gits/kraken_setup.sh
+++ install_gits/kraken_setup.sh
@@ -1,8 +1,8 @@
-#1/bin/bash
+#!/bin/bash
 
 #Run on Raspberry Pi 5 to either install, update, or provide a clean install of kraken_sdr
 
-ACTION = "$1"
+ACTION ="$1"
 
 # ---------------
 
@@ -14,7 +14,7 @@ fi
 
 
 #if clean install, give warning on 
-if [[$ACTION == "clean_install"]]; then
+if [[ $ACTION == "clean_install" ]]; then
     echo "WARNING: A clean install will delete the folder with all of the contents of the Kraken_SDR respository!"
     read -p "Clean Install? (y/n): " ans
         if [[ ! "$ans" =~ ^[Yy]$ ]]; then
@@ -40,13 +40,13 @@ if [[$ACTION == "clean_install"]]; then
 
 fi
 
-if [["$ACTION" == "clean_install" || "install"]]; then
+if [[ "$ACTION" == "clean_install" || "$ACTION" == "install" ]]; then
 
     wget https://raw.githubusercontent.com/krakenrf/krakensdr_docs/main/install_scripts/krakensdr_aarch64_install_doa.sh
     sudo chmod +x krakensdr_aarch64_install_doa.sh
 
 
-if
+fi
 
 #regardless of action, run script:
 ./krakensdr_aarch64_install_doa.sh
